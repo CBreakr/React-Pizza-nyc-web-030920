dcdf99f638e034463eb87e6e6295ec85ec0940b7
CBreakr <github email address>
2020-05-16 19:40:13 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,81 @@ import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
 class App extends Component {
+
+  state = {
+    pizzas: [],
+    currentPizzaId: null
+  }
+
+  componentDidMount(){
+    // fetch the pizzas
+    this.fetchPizzas();
+  }
+
+  fetchPizzas = () => {
+    fetch("http://localhost:3000/pizzas")
+    .then(res => res.json())
+    .then(data => {
+      this.setState({pizzas: data});
+    })
+    .catch(err => console.error("err", err));
+  }
+
+  setCurrentPizza = (event) => {
+    console.log("set pizza", event.target.dataset.id);
+    if(event.target.dataset.id){
+      this.setState({currentPizzaId: event.target.dataset.id});
+    }
+  }
+
+  patchPizza = (pizza) => {
+
+    console.log("patch pizza", pizza);
+
+    fetch(`http://localhost:3000/pizzas/${this.state.currentPizzaId}`, {
+      method:"PATCH",
+      headers: {
+        "content-type": "application/json",
+        "accept": "application/json"
+      },
+      body: JSON.stringify(pizza)
+    })
+    .then(res => res.json())
+    .then(data => {
+      this.fetchPizzas();
+    })
+    .catch(err => console.error("err", err));
+  }
+
+  findCurrentPizza = () => {
+    if(this.state.currentPizzaId){
+      // allow for breaking out of loop early
+      for(let i = 0; i < this.state.pizzas.length; i++){
+        const nextPizza = this.state.pizzas[i];
+        // I'm not going to do an extra conversion anywhere for the sake of this
+        if(nextPizza.id == this.state.currentPizzaId){
+          return nextPizza;
+        }
+      }
+    }
+    return {
+      id: null,
+      topping: "",
+      size: null,
+      vegetarian: false
+    };
+  }
+
   render() {
+
+    const currentPizza = this.findCurrentPizza();
+    console.log("current pizza", currentPizza);
+
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm {...currentPizza} patchPizza={this.patchPizza}/>
+        <PizzaList pizzas={this.state.pizzas} setCurrentPizza={this.setCurrentPizza}/>
       </Fragment>
     );
   }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,16 +1,66 @@
 import React from "react"
 
-const PizzaForm = () => {
-  return(
+class PizzaForm extends React.Component {
+
+  state = {
+    id: null,
+    topping: "",
+    size: "",
+    vegetarian: false
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(this.props.id !== prevProps.id){
+      this.setState({
+        id: this.props.id,
+        topping: this.props.topping,
+        size: this.props.size,
+        vegetarian: this.props.vegetarian
+      });
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log("input change", event.target.value);
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  onRadioChange = (event) => {
+    this.setState({
+      vegetarian: event.target.value === "Vegetarian"
+    });
+  }
+
+  updatePizza = () => {
+    this.props.patchPizza(this.state);
+
+    this.setState({
+      id: null,
+      topping: "",
+      size: "",
+      vegetarian: false
+    });
+  }
+
+  render() {
+
+    console.log("form props", this.props);
+    console.log("form state", this.state);
+
+    return(
       <div className="form-row">
         <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
+            <input type="text" className="form-control" name="topping" placeholder="Pizza Topping" value={
                 //Pizza Topping Should Go Here
-                null
-              }/>
+                this.state.topping
+              }
+              onChange={this.onInputChange}
+              />
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select value={this.state.size} className="form-control" name="size" onChange={this.onInputChange}>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -18,24 +68,30 @@ const PizzaForm = () => {
         </div>
         <div className="col">
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
+            <input className="form-check-input" type="radio" value="Vegetarian" group="vegetarian" 
+              checked={this.state.vegetarian}
+              onChange={this.onRadioChange}
+            />
             <label className="form-check-label">
               Vegetarian
             </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
+            <input className="form-check-input" type="radio" value="Not Vegetarian" group="vegetarian" 
+              checked={!this.state.vegetarian}
+              onChange={this.onRadioChange}
+            />
             <label className="form-check-label">
               Not Vegetarian
             </label>
           </div>
         </div>
         <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+          <button type="submit" className="btn btn-success" onClick={this.updatePizza}>Submit</button>
         </div>
       </div>
-
-  )
+    )
+  }
 }
 
 export default PizzaForm
